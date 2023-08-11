f6900ee7d65c9768b66b5fd509fe8842b21c53b6
gisacc54 <giftisacc54@gmail.com>
2023-08-11 20:33:11 +0300

    first commit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -17,10 +17,10 @@ int main(void)
 x = n % 10;
 printf("Last digit of %d is %d ", n, x);
 if (x > 5)
-printf("and is greater than 5");
+printf("and is greater than 5n");
 else if (x == 0)
-printf("and is 0");
+printf("and is 0\n");
 else
-printf("and is less than 6 and not 0");
+printf("and is less than 6 and not 0\n");
 return (0);
 }
