f363e17c4c7f85ad5508f5613a207e891bb9a9bd
gisacc54 <giftisacc54@gmail.com>
2023-08-11 20:42:36 +0300

    first commit

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+*   main - This is main function
+*
+*   Return: 0
+*/
+int main(void)
+{
+char myChar;
+for (myChar = 'a'; myChar <= 'z'; myChar++)
+{
+if(myChar == 'e' || myChar == 'q')
+continue;
+putchar(myChar);
+}
+putchar('\n');
+return (0);
+}
