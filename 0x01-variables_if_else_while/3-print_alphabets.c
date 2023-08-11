45873a29e6c0feb3c2cd076df1fdae42913c5a3f
gisacc54 <giftisacc54@gmail.com>
2023-08-11 20:36:51 +0300

    first commit

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+*   main - This is main function
+*
+*   Return: 0
+*/
+int main(void)
+{
+char myChar;
+for (myChar = 'a'; myChar <= 'Z'; myChar++)
+{
+putchar(myChar);
+}
+putchar('\n');
+return (0);
+}
