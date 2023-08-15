f66e0041fc4062f3866ad33aaf6574db08dabd1e
gisacc54 <giftisacc54@gmail.com>
2023-08-16 02:24:51 +0300

    update

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -10,7 +10,7 @@ void print_to_98(int n)
 int i;
 if (n > 98)
 {
-for (i = n; i > 98; i--)
+for (i = n; i >= 98; i--)
 {
 if (i == 98)
 printf("%d\n", i);
@@ -20,7 +20,7 @@ printf("%d, ", i);
 }
 else
 {
-for (i = n; i < 98; i++)
+for (i = n; i <= 98; i++)
 {
 if (i == 98)
 printf("%d\n", i);
