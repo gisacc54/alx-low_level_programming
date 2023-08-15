3148d22f41fc0b7fc38cb2973fd39e461809eeda
gisacc54 <giftisacc54@gmail.com>
2023-08-16 00:51:37 +0300

    update

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -3,11 +3,12 @@
 
 /**
 *   _islower - This is _islower fuction
+*   @c: char to cheack
 *   Return: 0
 */
 int _islower(int c)
 {
-if (c>='a' && c<='z')
+if (c >= 'a' && c <= 'z')
 return (1);
 else
 return (0);
