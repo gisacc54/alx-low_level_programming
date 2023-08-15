0a6def266f963a8ec4a158021af5de4dc3ca59b4
gisacc54 <giftisacc54@gmail.com>
2023-08-16 01:02:59 +0300

    update

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+
+/**
+*   print_sign - This is print_sign fuction
+*   @n: int to cheack
+*   Return: 0
+*/
+int print_sign(int n)
+{
+if (n > 0)
+{
+return (1);
+}
+else if (n < 0)
+return (-1);
+else
+return (0);
+}
