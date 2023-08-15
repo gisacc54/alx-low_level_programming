2eb4d2fb48ad787c821aac6ebc8c974651b01eb2
gisacc54 <giftisacc54@gmail.com>
2023-08-16 00:22:41 +0300

    update

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+
+/**
+*   print_alphabet - This is print_alphabet fuction
+*/
+void print_alphabet(void)
+{
+char myChar;
+for (myChar = 'a'; i < 'z'; i++)
+{
+_putchar(myChar);
+}
+_putchar('\n');
+}
