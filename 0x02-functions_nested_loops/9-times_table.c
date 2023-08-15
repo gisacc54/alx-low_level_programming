ae16d01bbe962b10d0934e54a3cbc1ca077f306f
gisacc54 <giftisacc54@gmail.com>
2023-08-16 02:00:45 +0300

    update

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -12,9 +12,18 @@ int h, m,k;
 	{
 		for (m = 0; m < 9; m++)
 		{
+			if(m==0)
+			_putchar(m)
+
             k = h*m;
 			_putchar(k + '0');
             _putchar(',');
+			if (k <= 9)
+				_putchar(' ');
+			else
+				_putchar((k / 10) + '0');
+
+			_putchar((k % 10) + '0');
 		}
         _putchar('\n');
 	}
