9c499e3346cabd362d16a91a3664ee7b0ae0f6b2
gisacc54 <giftisacc54@gmail.com>
2023-08-16 01:55:15 +0300

    update

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+
+/**
+*   times_table - This is print_last_digit fuction
+*/
+void times_table(void)
+{
+int h, m;
+
+	for (h = 0; h < 9; h++)
+	{
+		for (m = 0; m < 9; m++)
+		{
+            k == h*m;
+			_putchar(k + '0');
+            _putchar(',');
+		}
+	}
+}
