9ee6a2c3be1e8263313b9f1a463234cdfcd23d83
gisacc54 <giftisacc54@gmail.com>
2023-08-16 02:24:05 +0300

    update

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -13,9 +13,9 @@ if (n > 98)
 for (i = n; i > 98; i--)
 {
 if (i == 98)
-printf("%d\n", i)
+printf("%d\n", i);
 else
-printf("%d, ", i)
+printf("%d, ", i);
 }
 }
 else
@@ -23,9 +23,9 @@ else
 for (i = n; i < 98; i++)
 {
 if (i == 98)
-printf("%d\n", i)
+printf("%d\n", i);
 else
-printf("%d, ", i)
+printf("%d, ", i);
 }
 }
 }
