414cf45d2cced245969e6d4c5a07739bbe603593
gisacc54 <giftisacc54@gmail.com>
2023-08-18 21:53:33 +0300

    update

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,43 @@
+#include "main.h"
+
+/**
+ * print_square - This is print_square fuction
+ * @size: times n
+ *
+ */
+void print_triangle(int size)
+
+{
+
+
+int i, z, k, p;
+
+if (size > 0)
+{
+k = size - 1;
+for (i = 0; i < size ; i++)
+{
+for (z = k; z > 0 ; z--)
+{
+_putchar (' ');
+}
+
+for (p = 0; p <= i; p++)
+{
+_putchar (35);
+}
+
+k--;
+
+_putchar ('\n');
+
+
+}
+
+}
+
+else
+{
+_putchar ('\n');
+}
+}
