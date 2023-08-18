f8fd21bc52640423f80358f20e4d76eee8d769f4
gisacc54 <giftisacc54@gmail.com>
2023-08-18 21:12:50 +0300

    update

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -5,10 +5,13 @@
 */
 void print_numbers(void)
 {
-char i;
-for (i = '0'; i < '10'; i++)
+int i;
+for (i = 0; i < 10; i++)
 {
-_putchar(i+'0')
+if (i != 2 && i != 4)
+{
+_putchar(i + '0');
+}
 }
 _putchar('\n');
 }
