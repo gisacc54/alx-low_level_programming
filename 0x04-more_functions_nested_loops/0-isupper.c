3223deef90e7ed9d73d352bf53c70e328200076a
gisacc54 <giftisacc54@gmail.com>
2023-08-18 20:24:08 +0300

    update

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+*   _isupper - This is _isupper fuction
+*   @c: char to cheack
+*   Return: 0 or 1
+*/
+int _isupper(int c)
+{
+if (c >= 'a' && c <= 'z')
+return (0);
+
+return (1);
+}
