c516a289aad84ea9d9c2280ebdded967c2233b06
gisacc54 <giftisacc54@gmail.com>
2023-08-18 21:42:01 +0300

    update

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_line - This is print_line fuction
+ * @n: times n
+ *
+ */
+
+
+void print_line(int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		_putchar('_');
+	}
+	_putchar('\n');
+}
