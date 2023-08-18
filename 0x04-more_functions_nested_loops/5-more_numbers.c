2144a692b094e2fe5835e89fca6da00672452783
gisacc54 <giftisacc54@gmail.com>
2023-08-18 21:37:33 +0300

    update

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+*   more_numbers - This is more_numbers fuction
+*/
+void more_numbers(void)
+{
+int i;
+int j;
+for (i = 0; i < 10; i++)
+{
+for (j = 0; j < 10; j++)
+{
+_putchar(j + '0');
+}
+_putchar('\n');
+}
+
+}
