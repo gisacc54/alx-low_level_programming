b4754691f0ab12a418e68166b0280c307387b777
gisacc54 <giftisacc54@gmail.com>
2023-08-18 21:48:56 +0300

    update

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_square - This is print_square fuction
+ * @size: times n
+ *
+ */
+void print_square(int size)
+{
+int k, m;
+	if (size <= 0)
+	{
+		_putchar('\n');
+	} else
+	{
+		for (k = 0; k < skze; k++)
+		{
+			for (m = 0; m < size; m++)
+			{
+				_putchar('#');
+			}
+			_putchar('\n');
+		}
+	}
+}
