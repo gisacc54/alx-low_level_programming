756a00f74b63873f01e4fc6da23c293ae607c8a5
gisacc54 <giftisacc54@gmail.com>
2023-08-24 22:44:52 +0300

    update

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+*   _strcat - This is _strcat fuction
+*   @dest: pointer
+*   @src: pointer
+*   Return: *char
+*/
+char *_strcat(char *dest, char *src)
+{
+char *dest_new = dest;
+
+while (*dest != '\0')
+{
+dest_new++;
+}
+
+while (*src != '\0')
+{
+*dest_new = *src;
+src++;
+dest_new++;
+}
+*dest_new = '\0'
+return (dest);
+}
