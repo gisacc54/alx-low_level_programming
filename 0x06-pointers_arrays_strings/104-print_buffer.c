5a12a805f13deeada29b0414868a970d1dc430ce
gisacc54 <giftisacc54@gmail.com>
2023-08-25 01:09:01 +0300

    update

--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -1,7 +1,48 @@
 #include "main.h"
 
 /**
-*   rot13 - This is rot13 fuction
-*   @str: pointer
-*   Return: *char
+*   print_buffer - This is print_buffer fuction
+*   @b: pointer
+*   @size: int
 */
+void print_buffer(char *b, int size)
+{
+if (size <= 0)
+{
+printf("\n");
+return;
+}
+
+for (int i = 0; i < size; i += 10)
+{
+printf("%08x: ", i);
+
+for (int j = 0; j < 10; j++)
+{
+if (i + j < size)
+{
+printf("%02x", (unsigned char)b[i + j]);
+}
+else
+{
+printf("  ");
+}
+
+if (j % 2 == 1)
+{
+printf(" ");
+}
+}
+
+printf(" ");
+for (int j = 0; j < 10; j++)
+{
+if (i + j < size)
+{
+printf("%c", isprint(b[i + j]) ? b[i + j] : '.');
+}
+}
+
+printf("\n");
+}
+}
