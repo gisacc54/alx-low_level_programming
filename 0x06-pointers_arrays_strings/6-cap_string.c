d731060a080d1f0efde2f8ccd1c85cb35819fb6b
gisacc54 <giftisacc54@gmail.com>
2023-08-24 23:55:25 +0300

    update

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,40 @@
+#include "main.h"
+#include <stdbool.h>
+#include <ctype.h>
+
+/**
+*   cap_string - This is cap_string fuction
+*   @str: pointer
+*   Return: *char
+*/
+char *cap_string(char *str) {
+char *ptr = str;
+bool new_word = true;
+
+while (*ptr != '\0') {
+if (is_separator(*ptr)) {
+new_word = true;
+} else if (new_word && isalpha(*ptr)) 
+{
+*ptr = toupper((unsigned char)*ptr);
+new_word = false;
+} else {
+*ptr = tolower((unsigned char)*ptr);
+}
+ptr++;
+}
+
+return str;
+}
+
+bool is_separator(char c) {
+char separators[] = " \t\n,;.!?\"(){}";
+
+for (int i = 0; separators[i] != '\0'; i++) {
+if (c == separators[i]) {
+return true;
+}
+}
+
+return false;
+}
