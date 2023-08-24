4d8f479d0ed4104aa4390da68544027a65397528
gisacc54 <giftisacc54@gmail.com>
2023-08-25 00:44:14 +0300

    update

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -1,7 +1,12 @@
 #include "main.h"
 
 /**
-*   rot13 - This is rot13 fuction
-*   @str: pointer
+*   infinite_add - This is infinite_add fuction
+*   @n1: pointer
+*   @n2: pointer
+*   @r: pointer
+*   @size_r: int
 *   Return: *char
 */
+
+char *infinite_add(char *n1, char *n2, char *r, int size_r);
