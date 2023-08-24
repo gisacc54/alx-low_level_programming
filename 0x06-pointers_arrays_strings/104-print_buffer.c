037d0e19d6b29796b78d1be81284fdbade5f4a27
gisacc54 <giftisacc54@gmail.com>
2023-08-25 01:13:53 +0300

    update

--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -1,48 +1,54 @@
 #include "main.h"
 
 /**
+*   printl - This is printl fuction
+*   @c: pointer
+*   @s: int
+*   @l: int
+*/
+void printl(char *c, int s, int l)
+{
+int j, k;
+for (j = 0; j <= 9; j++)
+{
+if (j <= s)
+printf("%02x", c[l * 10 + j]);
+else
+printf("  ");
+if (j % 2)
+putchar(' ');
+}
+for (k = 0; k <= s; k++)
+{
+if (c[l * 10 + k] > 31 && c[l * 10 + k] < 127)
+putchar(c[l * 10 + k]);
+else
+putchar('.');
+}
+}
+
+
+/**
 *   print_buffer - This is print_buffer fuction
 *   @b: pointer
 *   @size: int
 */
 void print_buffer(char *b, int size)
 {
-if (size <= 0)
+int i;
+for (i = 0; i <= (size - 1) / 10 && size; i++)
 {
-printf("\n");
-return;
-}
-
-for (int i = 0; i < size; i += 10)
+printf("%08x: ", i * 10);
+if (i < size / 10)
 {
-printf("%08x: ", i);
-
-for (int j = 0; j < 10; j++)
-{
-if (i + j < size)
-{
-printf("%02x", (unsigned char)b[i + j]);
+printl(b, 9, i);
 }
 else
 {
-printf("  ");
+printl(b, size % 10 - 1, i);
 }
-
-if (j % 2 == 1)
-{
-printf(" ");
+putchar('\n');
 }
-}
-
-printf(" ");
-for (int j = 0; j < 10; j++)
-{
-if (i + j < size)
-{
-printf("%c", isprint(b[i + j]) ? b[i + j] : '.');
-}
-}
-
-printf("\n");
-}
+if (size == 0)
+putchar('\n');
 }
