f3e70242429c70be13ed09af76d32b424986530a
gisacc54 <giftisacc54@gmail.com>
2023-08-24 23:59:12 +0300

    update

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -7,34 +7,48 @@
 *   @str: pointer
 *   Return: *char
 */
-char *cap_string(char *str) {
+char *cap_string(char *str)
+{
 char *ptr = str;
 bool new_word = true;
 
-while (*ptr != '\0') {
-if (is_separator(*ptr)) {
+while (*ptr != '\0')
+{
+if (is_separator(*ptr))
+{
 new_word = true;
-} else if (new_word && isalpha(*ptr)) 
+}
+else if (new_word && isalpha(*ptr))
 {
 *ptr = toupper((unsigned char)*ptr);
 new_word = false;
-} else {
+}
+else
+{
 *ptr = tolower((unsigned char)*ptr);
 }
 ptr++;
 }
 
-return str;
+return (str);
 }
 
-bool is_separator(char c) {
+/**
+*   is_separator - This is is_separator fuction
+*   @c: pointer
+*   Return: bool
+*/
+bool is_separator(char c)
+{
 char separators[] = " \t\n,;.!?\"(){}";
 
-for (int i = 0; separators[i] != '\0'; i++) {
-if (c == separators[i]) {
-return true;
+for (int i = 0; separators[i] != '\0'; i++)
+{
+if (c == separators[i])
+{
+return (true);
 }
 }
 
-return false;
+return (false);
 }
