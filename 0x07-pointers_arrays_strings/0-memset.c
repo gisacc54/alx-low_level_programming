53969da05500c33f900e65a46771de4319287101
gisacc54 <giftisacc54@gmail.com>
2023-08-29 00:50:52 +0300

    update

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -9,9 +9,12 @@
 */
 char *_memset(char *s, char b, unsigned int n)
 {
-for (unsigned int i = 0; i < n; ++i)
+unsigned int j = 0;
+
+while (j < n)
 {
-s[i] = b[i];
+s[j] = b;
+j++;
 }
 
 return (s);
