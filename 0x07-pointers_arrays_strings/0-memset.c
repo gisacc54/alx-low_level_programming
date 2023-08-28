03bd760ff64987b153cc9c236c18135cf114b5ac
gisacc54 <giftisacc54@gmail.com>
2023-08-29 00:43:34 +0300

    update

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+*   _memset - This is _memset fuction
+*   @s: pointer s
+*   @b: char
+*   @n: int
+*   Return: chaar
+*/
+char *_memset(char *s, char b, unsigned int n)
+{
+unsigned int j = 0;
+
+whle(j < n)
+{
+s[j] = b;
+j++;
+}
+
+return (s);
+}
