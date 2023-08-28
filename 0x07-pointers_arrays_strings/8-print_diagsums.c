70c83dd605d4cc72d1bcc79b3553f544874a81ca
gisacc54 <giftisacc54@gmail.com>
2023-08-29 01:40:09 +0300

    update

---
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+*   print_diagsums - This is print_diagsums fuction
+*   @a: pointer a
+*   @size: int
+*/
+void print_diagsums(int *a, int size)
+{
+int i, S1, S2;
+
+S1 = 0;
+S2 = 0;
+
+for (i = 0; i < (size * size); i++)
+{
+if (i % (size + 1) == 0)
+S1 += a[i];
+if (i % (size - 1) == 0 && i != 0 && i < size * size - 1)
+S2 += a[i];
+}
+printf("%d, %d\n", S1, S2);
+}
