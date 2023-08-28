c905fe8e5d762adf1c8c6c32f8154d2365f4e3f9
gisacc54 <giftisacc54@gmail.com>
2023-08-29 01:43:45 +0300

    update

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
 *   _strspn - This is _strspn fuction
--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
 *   _strpbrk - This is _strpbrk fuction
