a187fbf981ef6097a10c86e09581f06904bb1988
gisacc54 <giftisacc54@gmail.com>
2023-08-29 01:54:08 +0300

    update

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -1,5 +1,5 @@
 #include "main.h"
-
+#include <stdio.h>
 /**
 *   _strchr - This is _strchr fuction
 *   @s: pointer s
@@ -8,13 +8,18 @@
 */
 char *_strchr(char *s, char c)
 {
-while (*s != '\0')
+int a;
+
+while (1)
 {
-if (*s == c)
+a = *s++;
+if (a == c)
 {
-return (s);
+return (s - 1);
 }
-s++;
-}
+if (a == 0)
+{
 return (NULL);
+}
+}
 }
